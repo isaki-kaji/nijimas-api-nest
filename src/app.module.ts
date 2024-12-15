c87c379859dfbb20928197832df3358f256d9921
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2024-12-15 17:47:14 +0900

    config: dbの接続設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DbModule } from 'db/db.module';
+import { EnvModule } from 'env/env.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EnvModule, DbModule],
 })
 export class AppModule {}
---
+++ src/db/data-source.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import * as dotenvExpand from 'dotenv-expand';
+
+// .envファイルの内容をprocess.envに展開する
+dotenvExpand.expand(dotenv.config());
+
+export default new DataSource({
+  type: 'postgres',
+  url: process.env.DATASOURCE_URL,
+  entities: ['dist/domain/**/*.entity.js'],
+  migrations: ['dist/database/migrations/*.js'],
+});
