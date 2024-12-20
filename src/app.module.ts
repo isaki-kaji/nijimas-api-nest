323a56d5f1f382b1fdd02d2679300fe88a4d814a
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2024-12-20 09:27:44 +0900

    feat: グローバルなバリデーションパイプを適用

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { EnvModule } from 'env/env.module';
 import { UsersModule } from './domain/users/users.module';
 import { AppConfigModule } from 'config/app-config.module';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { AuthGuard } from 'common/guard/auth.guard';
 import { UidInterceptor } from 'common/interceptor/uid.interceptor';
+import { PostsModule } from './domain/posts/posts.module';
 
 @Module({
-  imports: [EnvModule, AppConfigModule, UsersModule],
+  imports: [EnvModule, AppConfigModule, UsersModule, PostsModule],
   providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
