4776f897368aae9f0c498636b5f4cdaedff54cf9
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2024-12-21 17:01:17 +0900

    test: UserServiceのUTを実装

--- src/domain/users/users.service.spec.ts
+++ src/domain/users/users.service.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { faker } from '@faker-js/faker';
+import { User } from './entities/user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common/exceptions';
 
 describe('UsersService', () => {
   let service: UsersService;
+  const usersRepository = {
+    create: jest.fn(),
+    findByUid: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: UsersRepository,
+          useValue: usersRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -15,4 +34,79 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('create', () => {
+    describe('when no error occurs', () => {
+      it('should create the user', async () => {
+        const uid = genUid();
+        const dto = genCreateDto(uid);
+        const user = genUser(dto);
+
+        usersRepository.findByUid.mockResolvedValueOnce(null);
+
+        await service.create(dto);
+
+        expect(usersRepository.findByUid).toHaveBeenCalledWith(uid);
+        expect(usersRepository.create).toHaveBeenCalledWith(user);
+      });
+    });
+
+    describe('when user already exists', () => {
+      it('should throw the exception', async () => {
+        const uid = genUid();
+        const dto = genCreateDto(uid);
+        const user = genUser(dto);
+
+        usersRepository.findByUid.mockResolvedValueOnce(user);
+
+        await expect(service.create(dto)).rejects.toThrow(ConflictException);
+        expect(usersRepository.findByUid).toHaveBeenCalledWith(uid);
+      });
+    });
+  });
+
+  describe('findByUid', () => {
+    describe('when user exists', () => {
+      it('should return the user', async () => {
+        const uid = genUid();
+        const expectedUser = genUser(uid);
+
+        usersRepository.findByUid.mockResolvedValueOnce(expectedUser);
+
+        const user = await service.findByUid(uid);
+
+        expect(user).toBe(expectedUser);
+      });
+    });
+
+    describe('when user not exists', () => {
+      it('should throw the exception', async () => {
+        const uid = genUid();
+
+        usersRepository.findByUid.mockResolvedValueOnce(null);
+
+        await expect(service.findByUid(uid)).rejects.toThrow(NotFoundException);
+        expect(usersRepository.findByUid).toHaveBeenCalledWith(uid);
+      });
+    });
+  });
 });
+
+const genUid = () => faker.string.alphanumeric(28);
+
+const genCreateDto = (uid: string): CreateUserDto => ({
+  uid,
+  username: faker.person.firstName(),
+});
+
+const genUser = (input: string | CreateUserDto | UpdateUserDto): User => {
+  if (typeof input === 'string') {
+    return {
+      ...genCreateDto(input),
+    } as User;
+  } else {
+    return {
+      ...input,
+    } as User;
+  }
+};
