0e7c3bae017e9fad45f20c044d387a9191ed84c3
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-02-08 17:41:50 +0900

    fix: 軽微な修正

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -3,11 +3,11 @@ import * as dotenv from 'dotenv';
 import * as dotenvExpand from 'dotenv-expand';
 
 // .envファイルの内容をprocess.envに展開する
-dotenvExpand.expand(dotenv.config());
+// dotenvExpand.expand(dotenv.config());
 
-export default new DataSource({
-  type: 'postgres',
-  url: process.env.DATASOURCE_URL,
-  entities: ['dist/entities/*.entity.js'],
-  migrations: ['dist/database/migrations/*.js'],
-});
+// export default new DataSource({
+//   type: 'postgres',
+//   url: process.env.DATASOURCE_URL,
+//   entities: ['dist/entities/*.entity.js'],
+//   migrations: ['dist/database/migrations/*.js'],
+// });
--- test/setup.ts
+++
@@ -1,4 +0,0 @@
-import * as dotenv from 'dotenv';
-import * as dotenvExpand from 'dotenv-expand';
-
-dotenvExpand.expand(dotenv.config());
