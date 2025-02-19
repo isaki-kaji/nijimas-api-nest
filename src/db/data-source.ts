3fe25b2da31dbd44b979f1245d788156560519e9
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-02-19 23:47:49 +0900

    cicd: データソースを修正

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -3,14 +3,19 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
 import * as dotenvExpand from 'dotenv-expand';
 
-const envFile = '.env.test';
-
+const envFile =
+  process.env.ENV_FILE ||
+  (process.env.NODE_ENV === 'test' ? '.env.test' : '.env');
 const myEnv = dotenv.config({ path: envFile });
 dotenvExpand.expand(myEnv);
 
 const config: DataSourceOptions = {
   type: 'postgres',
-  url: process.env.DATASOURCE_URL,
+  host: process.env.DATASOURCE_HOST,
+  port: Number(process.env.DATASOURCE_PORT),
+  username: process.env.DATASOURCE_USERNAME,
+  password: process.env.DATASOURCE_PASSWORD,
+  database: process.env.DATASOURCE_DATABASE,
   migrations: ['src/db/migrations/*.ts'],
   entities: ['src/entities/*.entity.ts'],
 };
