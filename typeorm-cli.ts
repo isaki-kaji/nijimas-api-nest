418ca176e639dd617fad820c17a2c741c3fc25cd
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-02-19 21:34:35 +0900

    cicd: .env.testを指定するよう修正

--- typeorm-cli.ts
+++ typeorm-cli.ts
@@ -1,4 +1,10 @@
 import 'tsconfig-paths/register';
 import dataSource from './src/db/data-source';
 
+console.log('Using Database Config:');
+console.log('Host:', process.env.DATASOURCE_HOST);
+console.log('Port:', process.env.DATASOURCE_PORT);
+console.log('Username:', process.env.DATASOURCE_USERNAME);
+console.log('Database:', process.env.DATASOURCE_DATABASE);
+
 export default dataSource;
