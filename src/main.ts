a969e6a36be5e99ae7601349a0a9fc06dbf47ec8
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-03-23 00:06:10 +0900

    cicd: 起動時にログ出力

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,26 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  try {
+    console.log('NestJS server starting...');
 
-  app.use(
-    helmet({
-      contentSecurityPolicy: false,
-      frameguard: false,
-      hsts: { maxAge: 31536000, includeSubDomains: true },
-      noSniff: true,
-    }),
-  );
+    const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0');
+    app.use(
+      helmet({
+        contentSecurityPolicy: false,
+        frameguard: false,
+        hsts: { maxAge: 31536000, includeSubDomains: true },
+        noSniff: true,
+      }),
+    );
+
+    await app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0');
+    console.log('✅ NestJS is now listening');
+  } catch (error) {
+    console.error('🔥 NestJS failed to start:', error);
+    process.exit(1); // Cloud Run に「異常終了」として伝える
+  }
 }
+
 bootstrap();
