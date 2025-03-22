a0cf079f24c4908ee9e6b1bc7a1fb8f1681be187
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-03-22 15:44:19 +0900

    fix: 軽微な修正

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
   app.use(
     helmet({
       contentSecurityPolicy: false,
@@ -13,5 +13,7 @@ async function bootstrap() {
       noSniff: true,
     }),
   );
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
