7ae23eac120bb44df846c829014e84b049a9b677
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-03-27 15:00:31 +0900

    feat: いいねした投稿の取得について実装

--- src/modules/posts/infrastructure/posts.query.service.ts
+++ src/modules/posts/infrastructure/posts.query.service.ts
@@ -243,6 +243,68 @@ export class PostsQueryService implements IPostsQueryService {
     return rawPosts.map((raw) => this.toResponseDto(raw));
   }
 
+  async findFavoritePosts(
+    uid: string,
+    referencePostId?: string,
+  ): Promise<PostResponseDto[]> {
+    const sql = `
+      SELECT
+        p.post_id,
+        u.uid,
+        u.username,
+        u.profile_image_url,
+        p.main_category,
+        COALESCE(sc.sub_category1, '')::text AS sub_category1,
+        COALESCE(sc.sub_category2, '')::text AS sub_category2,
+        p.post_text,
+        p.photo_url,
+        p.expense,
+        p.location,
+        CASE WHEN f.uid IS NOT NULL THEN TRUE ELSE FALSE END AS is_favorite,
+        p.public_type_no,
+        p.created_at
+      FROM posts p
+      JOIN users u ON p.uid = u.uid
+      JOIN favorites f ON p.post_id = f.post_id
+      LEFT JOIN (
+        SELECT
+          ps.post_id,
+          MAX(CASE WHEN ps.category_no = '1' THEN s.category_name ELSE NULL END) AS sub_category1,
+          MAX(CASE WHEN ps.category_no = '2' THEN s.category_name ELSE NULL END) AS sub_category2
+        FROM post_subcategories ps
+        JOIN sub_categories s ON ps.category_id = s.category_id
+        GROUP BY ps.post_id
+      ) sc ON p.post_id = sc.post_id
+      WHERE
+        p.deleted_at IS NULL
+      AND
+        f.uid = $1
+      AND (
+        p.public_type_no = '0'
+        OR (
+          p.public_type_no = '1'
+          AND EXISTS (
+            SELECT 1
+            FROM follows
+            WHERE uid = $1 AND following_uid = p.uid
+          )
+        )
+        OR (
+          p.public_type_no IN ('1', '2')
+          AND p.uid = $1
+        )
+      )
+      ${referencePostId ? `AND p.post_id < $2` : ''}
+      ORDER BY p.post_id DESC
+      LIMIT 50;
+    `;
+
+    const params = referencePostId ? [uid, referencePostId] : [uid];
+    const rawPosts = await this.dataSource.query(sql, params);
+
+    return rawPosts.map((raw) => this.toResponseDto(raw));
+  }
+
   async findPostsBySubCategory(
     uid: string,
     categoryName: string,
