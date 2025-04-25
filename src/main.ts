26579235c57e84a89e252434ae60342039779bc3
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-04-25 12:23:59 +0900

    fix: swaggerの使用を一時中止

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 
 async function bootstrap() {
@@ -18,14 +17,14 @@ async function bootstrap() {
       }),
     );
 
-    const config = new DocumentBuilder()
-      .setTitle('Nijimas API')
-      .setDescription('Nijimas APIのドキュメント')
-      .setVersion('1.0')
-      .addBearerAuth()
-      .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api-docs', app, document);
+    // const config = new DocumentBuilder()
+    //   .setTitle('Nijimas API')
+    //   .setDescription('Nijimas APIのドキュメント')
+    //   .setVersion('1.0')
+    //   .addBearerAuth()
+    //   .build();
+    // const document = SwaggerModule.createDocument(app, config);
+    // SwaggerModule.setup('api-docs', app, document);
 
     await app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0');
     console.log('✅ NestJS is now listening');
